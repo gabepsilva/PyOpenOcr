6b6b73f9e050d45c1fe6d145c74b9f2db825dbe1
gabriel <gabrielpe@gmail.com>
2018-12-02 03:47:13 -0500

    modularized requirements.txt

--- Dockerfile
+++ Dockerfile
@@ -9,6 +9,7 @@ ENV DOCKER true
 
 ADD web/PyOpenOcr.py /opt/pyopenocr/PyOpenOcr.py
 ADD install/setup-env.sh /opt/pyopenocr/setup-env.sh
+ADD install/requirements.txt /opt/pyopenocr/requirements.txt
 ADD run_native.sh /opt/pyopenocr/run_native.sh
 
 RUN /opt/pyopenocr/setup-env.sh
--- install/setup-env.sh
+++ install/setup-env.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
+
+mkdir -p /opt/pyopenocr
+cp web/PyOpenOcr.py /opt/pyopenocr/PyOpenOcr.py
+cp install/requirements.txt /opt/pyopenocr/requirements.txt
+
 yum-config-manager --add-repo https://download.opensuse.org/repositories/home:/Alexander_Pozdnyakov/CentOS_7/
 rpm --import https://build.opensuse.org/projects/home:Alexander_Pozdnyakov/public_key
 yum -y install epel-release 
 yum -y install tesseract python36 python36-setuptools openssl
 easy_install-3.6 pip
-pip3 install flask flask_restful temp pyopenssl gunicorn
+pip3 install -r /opt/pyopenocr/requirements.txt
 
-mkdir -p /opt/pyopenocr
-cp web/PyOpenOcr.py /opt/pyopenocr/PyOpenOcr.py
 cd /opt/pyopenocr
-
 openssl genrsa 2048 > server.key
 openssl req -new -newkey rsa:4096 -key server.key -out server.csr -subj "/C=CA/ST=ON/L=Toronto/O=pyopenocr/CN=pyopenocr"
 openssl x509 -req -days 3650 -in server.csr -signkey server.key -out server.crt
