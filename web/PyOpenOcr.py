c9bdde47d5453d37971e2012111918b298edd1c9
gabriel <gabrielpe@gmail.com>
2018-12-02 13:19:49 -0500

    using tesseract api

--- web/PyOpenOcr.py
+++ web/PyOpenOcr.py
@@ -1,9 +1,11 @@
+from base64 import decodebytes
+from io import BytesIO
+
 from flask import Flask, request
 from flask_restful import Resource, Api
 
-import os
-import base64
-import tempfile
+import pytesseract
+from PIL import Image
 
 
 app = Flask(__name__)
@@ -12,23 +14,10 @@ api = Api(app)
 class PyOpenOcr(Resource):
 
     def post(self):
+        decoded = decodebytes(request.data)
+        dt = Image.open(BytesIO(decoded))
+        return pytesseract.image_to_string(dt)
 
-        decoded = base64.decodebytes(request.data)
-
-        tf = tempfile.NamedTemporaryFile(mode='w+b', prefix='oocr_', dir='/tmp', delete=False)
-        tf.write(decoded)
-        tf.close()
-
-        os.system(f'tesseract {tf.name} {tf.name} -l eng')
-
-        f = open(f'{tf.name}.txt', mode="r", encoding="utf-8")
-        ocr_txt = f.read()
-        f.close()
-
-        os.remove(tf.name)
-        os.remove(f'{tf.name}.txt')
-
-        return ocr_txt
 
 api.add_resource(PyOpenOcr, '/ocr-base64')
 
