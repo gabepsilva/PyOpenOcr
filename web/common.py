ce54a8bec12525afd4a2a48fb80f46a8022ab502
gabriel <gabrielpe@gmail.com>
2018-12-03 00:47:14 -0500

    lang specified

--- web/common.py
+++ web/common.py
@@ -6,4 +6,4 @@ from io import BytesIO
 def data_to_text(data: bytes) -> str:
 
     img = Image.open(BytesIO(data))
-    return pytesseract.image_to_string(img)
+    return pytesseract.image_to_string(img, lang='eng')
