6a2923868b66b8b92611a8a4eedda5a45fb2b66c
gabriel <gabrielpe@gmail.com>
2018-12-03 00:12:31 -0500

    small code refactor

--- web/PyOpenOcr.py
+++ web/PyOpenOcr.py
@@ -1,25 +1,33 @@
-from base64 import decodebytes
-from io import BytesIO
+import requests
+import base64
 
 from flask import Flask, request
 from flask_restful import Resource, Api
 
-import pytesseract
-from PIL import Image
+from web import common
 
 
 app = Flask(__name__)
 api = Api(app)
 
-class PyOpenOcr(Resource):
+class PyOpenOcrBase64(Resource):
 
     def post(self):
-        decoded = decodebytes(request.data)
-        dt = Image.open(BytesIO(decoded))
-        return pytesseract.image_to_string(dt)
+        decoded = base64.decodebytes(request.data)
+
+        return common.data_to_text(decoded)
+
+class PyOpenOcrURL(Resource):
+
+    def post(self):
+        url =  request.data.decode("utf-8")
+        data = requests.get(url, allow_redirects=True, verify=False)
+
+        return common.data_to_text(data.content)
 
 
-api.add_resource(PyOpenOcr, '/ocr-base64')
+api.add_resource(PyOpenOcrBase64, '/ocr-base64')
+api.add_resource(PyOpenOcrURL, '/ocr-url')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8443, ssl_context='adhoc')
